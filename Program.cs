67d086858558035d7a28ad51479314fdcc8c1869
Todd Mandell <todd.mandell@outlook.com>
2018-06-20 20:39:08 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,31 +8,29 @@ using System.Linq;
 
 namespace SNMP_Event_Sender
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+
+        public static void Main(string[] args)
         {
-
-            Console.WriteLine("Type the eventlog you wish to monitor --");
-            string SyslogType = Console.ReadLine();
-            Console.WriteLine("Type the minimum XML event viewer log level --");
-            string MinLogLevel = Console.ReadLine();
-
-        EventLogWatcher watcher = null;
+            EventLogWatcher watcher = null;
+            
+            //string IPAddr = args.ElementAt(1);
+            string SyslogType = args.First();
+            int MinLogLevel = Convert.ToInt32(args.Last());
 
         try
         {
+            Console.WriteLine("Log: " + args.First() + " -- Min Level: " + args.Last());
 
-            EventLogQuery subscriptionQuery = new EventLogQuery(SyslogType, PathType.LogName, "*[System/Level<"+MinLogLevel+"]");
-
+            //Min Log Level -- Higher Number is lower log level
+            EventLogQuery subscriptionQuery = new EventLogQuery(SyslogType, PathType.LogName, "*[System/Level<=" + MinLogLevel + "]");
             watcher = new EventLogWatcher(subscriptionQuery);
 
-            // Set watcher to listen for the EventRecordWritten
-            // event.  When this event happens, the callback method
             // (EventLogEventRead) will be called.
-            watcher.EventRecordWritten +=  new EventHandler<EventRecordWrittenEventArgs>(EventLogEventRead);
+            watcher.EventRecordWritten += new EventHandler<EventRecordWrittenEventArgs>(EventLogEventRead);
 
-            // Begin subscribing to events the events
+            // Begin subscribing to events
             watcher.Enabled = true;
 
             //Keep waiting for 25000 or more years
@@ -40,51 +38,47 @@ namespace SNMP_Event_Sender
             {
                 System.Threading.Thread.Sleep(900000000);
             }
+
         }
         catch (EventLogReadingException e)
         {
-            Console.WriteLine("Error reading the log: {0}", e.Message);
+            Console.WriteLine("Execute with arg 1 being System,Application,Security, etc and arg2 being the minimum log level OR Error reading the log: {0}", e.Message);
         }
         finally
         {
             // Stop listening to events
             watcher.Enabled = false;
-
             if (watcher != null)
             {
                 watcher.Dispose();
-            }
-        }
-    }
-
-    /// <summary>
-    /// Callback method that gets executed when an event is
-    /// reported to the subscription.
-    /// </summary>
-    public static void EventLogEventRead(object obj,
-        EventRecordWrittenEventArgs arg)
-    {
-        // Make sure there was no error reading the event.
-        if (arg.EventRecord != null)
-        {
-            //send the event data over udp to pre-specified SNMP message receiver at ipadd.parse address and port below
-            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram,
-            ProtocolType.Udp);
-            IPAddress serverAddr = IPAddress.Parse("!!!INSERT-THE-IP-ADDRESS-OF-SNMP-LISTENER-EMAILER-HOST-HERE!!!");
-            IPEndPoint endPoint = new IPEndPoint(serverAddr, 162);
-            string messedge = "EventID " + arg.EventRecord.Id + " " + arg.EventRecord.FormatDescription();
-            byte[] send_buffer = Encoding.ASCII.GetBytes(messedge);
-            sock.SendTo(send_buffer, endPoint);
-            sock.Close();
-            
-            Console.WriteLine("Received event {0} from the subscription.",arg.EventRecord.Id + " " + "Description: {0}", arg.EventRecord.FormatDescription());
-            
-        }
-        else
-        {
-            Console.WriteLine("The event instance was null.");
-        }
-
         }
     }
 }
+
+        public static void EventLogEventRead(object obj, EventRecordWrittenEventArgs arg)
+        {
+
+            // Make sure there was no error reading the event.
+            if (arg.EventRecord != null)
+            {
+                //send the event data over udp to pre-specified message receiver at ipadd.parse address and port below
+                Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram,
+                ProtocolType.Udp);
+                  IPAddress serverAddr = IPAddress.Parse("INSERT TARGET IP ADDRESS OF SNMP LISTENER EMAILER SERVER");
+                IPEndPoint endPoint = new IPEndPoint(serverAddr, 162);
+                string messedge = "EventID " + arg.EventRecord.Id + " " + arg.EventRecord.FormatDescription();
+                byte[] send_buffer = Encoding.ASCII.GetBytes(messedge);
+                sock.SendTo(send_buffer, endPoint);
+                sock.Close();
+
+                Console.WriteLine("Received event {0} at " + DateTime.Now, arg.EventRecord.Id, arg.EventRecord.FormatDescription());
+
+            }
+            else
+            {
+                Console.WriteLine("The event instance was null.");
+            }
+        }
+    }
+}
+
